b204c745be7e90fc3e096fdfd8f220c5546aeeb1
Marysia <mariabiss3012@gmail.com>
2026-03-06 12:40:30 +0100

    feat: add groups endpoints and group membership

---
+++ ExpenseSplitter.Api/DTOs/Requests/CreateGroupRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseSplitter.Api.DTOs.Requests;
+
+public class CreateGroupRequest
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(10)]
+    public string Currency { get; set; } = "PLN";
+
+    [Required]
+    public Guid OwnerId { get; set; }
+}
---
+++ ExpenseSplitter.Infrastructure/Migrations/20260306113555_AddGroups.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseSplitter.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGroups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
